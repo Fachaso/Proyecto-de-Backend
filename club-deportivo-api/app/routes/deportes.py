4d0a9881d94f95292089279d9b5a2cabc915daed
julietanagera <julietanagera@gmail.com>
2026-09-25 14:31:28 -0300

    Refactor get_deportes to use listar_deportes

--- club-deportivo-api/app/routes/deportes.py
+++ club-deportivo-api/app/routes/deportes.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, jsonify
-from app.services.deportes_service import DeportesService
+from app.services.deportes_service import listar_deportes
 
-deportes_bp = Blueprint('deportes', __name__, url_prefix='/deportes')
 
-@deportes_bp.route('', methods=['GET'])
+deportes_bp = Blueprint("deportes", __name__, url_prefix="/deportes")
+
+
+@deportes_bp.route("", methods=["GET"])
 def get_deportes():
-    respuesta, estado = DeportesService.listar_deportes()
-    
+    respuesta, estado = listar_deportes()
+
     if estado == 204 or not respuesta:
-        return '', 204
-        
+        return "", 204
+
     return jsonify(respuesta), estado
